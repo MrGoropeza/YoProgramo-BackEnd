ad13a90a623fadfb26dc4993d899cbcc0375ed2b
MrGoropeza <goropeza8@gmail.com>
2022-12-28 08:05:16 -0300

    update: estrategia de la primary key Identity -> sequence

--- src/main/java/com/mrgoropeza/portfoliobackend/model/Tecnologia.java
+++ src/main/java/com/mrgoropeza/portfoliobackend/model/Tecnologia.java
@@ -17,7 +17,7 @@ import lombok.Data;
 @Entity
 public class Tecnologia {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private String name;
     private String imageUrl;
