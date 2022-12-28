77081fe739b331dc5befb057b681a077458123d3
MrGoropeza <goropeza8@gmail.com>
2022-12-28 07:21:26 -0300

    add: clase para convertir objetos desde y hacia json

---
+++ src/main/java/com/mrgoropeza/portfoliobackend/utils/IJsonConverter.java
@@ -0,0 +1,10 @@
+package com.mrgoropeza.portfoliobackend.utils;
+
+public interface IJsonConverter {
+    static <T> T fromJsonString(String json, Class<T> type){
+        return null;
+    }
+    static String toJsonString(Object obj){
+        return null;
+    }
+}
---
+++ src/main/java/com/mrgoropeza/portfoliobackend/utils/JsonConverter.java
@@ -0,0 +1,87 @@
+package com.mrgoropeza.portfoliobackend.utils;
+
+import java.io.IOException;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.util.*;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.time.OffsetTime;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+
+public class JsonConverter implements IJsonConverter{
+
+    // Date-time helpers
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = new DateTimeFormatterBuilder()
+            .appendOptional(DateTimeFormatter.ISO_DATE_TIME)
+            .appendOptional(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+            .appendOptional(DateTimeFormatter.ISO_INSTANT)
+            .appendOptional(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SX"))
+            .appendOptional(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ssX"))
+            .appendOptional(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+            .toFormatter()
+            .withZone(ZoneOffset.UTC);
+
+    public static OffsetDateTime parseDateTimeString(String str) {
+        return ZonedDateTime.from(JsonConverter.DATE_TIME_FORMATTER.parse(str)).toOffsetDateTime();
+    }
+
+    private static final DateTimeFormatter TIME_FORMATTER = new DateTimeFormatterBuilder()
+            .appendOptional(DateTimeFormatter.ISO_TIME)
+            .appendOptional(DateTimeFormatter.ISO_OFFSET_TIME)
+            .parseDefaulting(ChronoField.YEAR, 2020)
+            .parseDefaulting(ChronoField.MONTH_OF_YEAR, 1)
+            .parseDefaulting(ChronoField.DAY_OF_MONTH, 1)
+            .toFormatter()
+            .withZone(ZoneOffset.UTC);
+
+    public static OffsetTime parseTimeString(String str) {
+        return ZonedDateTime.from(JsonConverter.TIME_FORMATTER.parse(str)).toOffsetDateTime().toOffsetTime();
+    }
+
+    private static ObjectReader reader;
+    private static ObjectWriter writer;
+
+    private static void instantiateMapper(Class<?> type) {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.findAndRegisterModules();
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        SimpleModule module = new SimpleModule();
+        module.addDeserializer(OffsetDateTime.class, new JsonDeserializer<OffsetDateTime>() {
+            @Override
+            public OffsetDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
+                String value = jsonParser.getText();
+                return JsonConverter.parseDateTimeString(value);
+            }
+        });
+        mapper.registerModule(module);
+        reader = mapper.readerFor(type);
+        writer = mapper.writerFor(type);
+    }
+
+    public static <T> T fromJsonString(String json, Class<T> type) throws IOException {
+        return getObjectReader(type).readValue(json);
+    }
+
+    public static String toJsonString(Object obj) throws JsonProcessingException {
+        return getObjectWriter(obj.getClass()).writeValueAsString(obj);
+    }
+
+    private static ObjectReader getObjectReader(Class<?> type) {
+        if (reader == null) instantiateMapper(type);
+        return reader;
+    }
+
+    private static ObjectWriter getObjectWriter(Class<?> type) {
+        if (writer == null) instantiateMapper(type);
+        return writer;
+    }
+    
+}
