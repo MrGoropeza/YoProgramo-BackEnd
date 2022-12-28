9586c02847aa302b3390e44329a65767e44b74ca
MrGoropeza <goropeza8@gmail.com>
2022-12-28 09:46:40 -0300

    add: mensaje de error por si no tipo de tecnologia que actualizar

--- src/main/java/com/mrgoropeza/portfoliobackend/controller/TipoTecnologiaController.java
+++ src/main/java/com/mrgoropeza/portfoliobackend/controller/TipoTecnologiaController.java
@@ -1,6 +1,7 @@
 package com.mrgoropeza.portfoliobackend.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.mrgoropeza.portfoliobackend.model.TipoTecnologia;
 import com.mrgoropeza.portfoliobackend.service.TipoTecnologiaService;
@@ -8,6 +9,7 @@ import com.mrgoropeza.portfoliobackend.service.TipoTecnologiaService;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,7 +36,9 @@ public class TipoTecnologiaController {
     @PutMapping(value="/tipoTecnologia/update/{id}")
     public TipoTecnologia editTipoTecnologia(@PathVariable Long id, @RequestBody TipoTecnologia tipoTecnologia) {
         TipoTecnologia tipo = tipoTecnologiaService.findTipoTecnologia(id);
-
+        if(tipo == null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Tipo de Tecnologia no encontrada");
+        }
         tipo.setName(tipoTecnologia.getName());
 
         tipoTecnologiaService.saveTipoTecnologia(tipo);
