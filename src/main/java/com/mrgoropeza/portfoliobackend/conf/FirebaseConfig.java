4820df1a5c01345e376330600af83818dba041af
MrGoropeza <goropeza8@gmail.com>
2023-01-01 04:34:06 -0300

    update: todas los secretos se obtienen de las variables de entorno

--- src/main/java/com/mrgoropeza/portfoliobackend/conf/FirebaseConfig.java
+++ src/main/java/com/mrgoropeza/portfoliobackend/conf/FirebaseConfig.java
@@ -1,15 +1,19 @@
 package com.mrgoropeza.portfoliobackend.conf;
 
-import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.Charset;
 
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.Resource;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
@@ -25,17 +29,15 @@ public class FirebaseConfig {
     @Value(value = "classpath:firebase.json")
     private Resource serviceAccountResource;
 
-    private String bucketName;
-
     private String imageUrl;
 
     @Bean
-    public FirebaseApp createFireBaseApp() throws IOException {
-        InputStream serviceAccount = serviceAccountResource.getInputStream();
+    public FirebaseApp createFireBaseApp() throws Exception {
+        InputStream serviceAccount = createFirebaseCredential();
 
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setStorageBucket(bucketName)
+                .setStorageBucket(environment.getRequiredProperty("FIREBASE_BUCKET_NAME"))
                 .build();
 
         if (FirebaseApp.getApps().size() > 0) {
@@ -55,6 +57,32 @@ public class FirebaseConfig {
     @Bean
     public String getImageUrl() {
         return imageUrl;
+    }
+
+    @Autowired
+    private Environment environment;
+
+    private InputStream createFirebaseCredential() throws Exception {
+        //private key
+        String privateKey = environment.getRequiredProperty("FIREBASE_PRIVATE_KEY").replace("\\n", "\n");
+    
+        FirebaseCredential firebaseCredential = new FirebaseCredential();
+        firebaseCredential.setType(environment.getRequiredProperty("FIREBASE_TYPE"));
+        firebaseCredential.setProject_id(environment.getRequiredProperty("FIREBASE_PROJECT_ID"));
+        firebaseCredential.setPrivate_key_id("FIREBASE_PRIVATE_KEY_ID");
+        firebaseCredential.setPrivate_key(privateKey);
+        firebaseCredential.setClient_email(environment.getRequiredProperty("FIREBASE_CLIENT_EMAIL"));
+        firebaseCredential.setClient_id(environment.getRequiredProperty("FIREBASE_CLIENT_ID"));
+        firebaseCredential.setAuth_uri(environment.getRequiredProperty("FIREBASE_AUTH_URI"));
+        firebaseCredential.setToken_uri(environment.getRequiredProperty("FIREBASE_TOKEN_URI"));
+        firebaseCredential.setAuth_provider_x509_cert_url(environment.getRequiredProperty("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"));
+        firebaseCredential.setClient_x509_cert_url(environment.getRequiredProperty("FIREBASE_CLIENT_X509_CERT_URL"));
+        //serialization of the object to json string
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = mapper.writeValueAsString(firebaseCredential);
+    
+        //convert jsonString string to InputStream using Apache Commons
+        return IOUtils.toInputStream(jsonString, Charset.defaultCharset());
     }
 
 }
