37ec1baa2301db9158a61bbacf9f1334a36674e8
MrGoropeza <goropeza8@gmail.com>
2023-01-02 03:12:52 -0300

    remove: referencia a firebase.json

--- src/main/java/com/mrgoropeza/portfoliobackend/conf/FirebaseConfig.java
+++ src/main/java/com/mrgoropeza/portfoliobackend/conf/FirebaseConfig.java
@@ -5,13 +5,11 @@ import java.nio.charset.Charset;
 
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.core.env.Environment;
-import org.springframework.core.io.Resource;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.auth.oauth2.GoogleCredentials;
@@ -25,9 +23,6 @@ import lombok.Data;
 @ConfigurationProperties(prefix = "firebase")
 @Data
 public class FirebaseConfig {
-
-    @Value(value = "classpath:firebase.json")
-    private Resource serviceAccountResource;
 
     private String imageUrl;
 
