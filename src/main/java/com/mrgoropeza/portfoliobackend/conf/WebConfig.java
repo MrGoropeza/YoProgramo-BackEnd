0182dacca0871fccd06fc16a2e45afe06129a726
MrGoropeza <goropeza8@gmail.com>
2023-01-08 16:21:20 -0300

    add: @CrossOrigin -> WebConfigurer

--- src/main/java/com/mrgoropeza/portfoliobackend/conf/WebConfig.java
+++ src/main/java/com/mrgoropeza/portfoliobackend/conf/WebConfig.java
@@ -1,20 +1,20 @@
 package com.mrgoropeza.portfoliobackend.conf;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
+@CrossOrigin
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedOriginPatterns("*")
                 .allowedMethods("*");
     }
 }
