60e3c3c1b4dcd913c320112be9e5a3350741e67e
MrGoropeza <goropeza8@gmail.com>
2023-01-13 20:27:10 -0300

    add: Project Model

---
+++ src/main/java/com/mrgoropeza/portfoliobackend/model/Project.java
@@ -0,0 +1,29 @@
+package com.mrgoropeza.portfoliobackend.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+
+@Data
+@Entity
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+    private String url;
+    @ManyToMany()
+    private List<Person> collab;
+    @ManyToMany()
+    private List<Tech> techs;
+    @OneToOne()
+    @JoinColumn()
+    private Place place;
+}
